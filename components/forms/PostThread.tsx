1bfec1b43f8d4702bebbf27b5680ac201d4806a5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-08-20 17:11:37 +0530

    Update PostThread.tsx

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,6 +1,11 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useOrganization } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+
 import {
   Form,
   FormControl,
@@ -9,23 +14,26 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import * as z from 'zod';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { usePathname, useRouter } from "next/navigation";
-import { updateUser } from "@/lib/actions/user.actions";
+
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
 
-const PostThread = ({userId}: { userId: string }) => {
+interface Props {
+  userId: string;
+}
+
+function PostThread({ userId }: Props) {
   const router = useRouter();
   const pathname = usePathname();
 
-  const form = useForm({
+  const { organization } = useOrganization();
+
+  const form = useForm<z.infer<typeof ThreadValidation>>({
     resolver: zodResolver(ThreadValidation),
     defaultValues: {
-      thread: '',
+      thread: "",
       accountId: userId,
     },
   });
@@ -34,35 +42,41 @@ const PostThread = ({userId}: { userId: string }) => {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
-    router.push('/');
-  }
+
+    router.push("/");
+  };
 
   return (
     <Form {...form}>
-      <form 
-        onSubmit={form.handleSubmit(onSubmit)} 
-        className="flex flex-col justify-start gap-10 mt-10"
+      <form
+        className='mt-10 flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
       >
         <FormField
           control={form.control}
-          name="thread"
+          name='thread'
           render={({ field }) => (
-            <FormItem className="flex gap-2 w-full flex-col">
-              <FormLabel className="text-base-semibold text-light-2">Content</FormLabel>
-              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Content
+              </FormLabel>
+              <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
                 <Textarea rows={15} {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" className="bg-primary-500">Post Thread</Button>
+
+        <Button type='submit' className='bg-primary-500'>
+          Post Thread
+        </Button>
       </form>
     </Form>
   );
-};
+}
 
 export default PostThread;
